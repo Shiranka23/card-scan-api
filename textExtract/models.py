0d1295e8f3af37e26383f443ea1ac1ad26e54c1d
shivansh <shivanshkate@gmail.com>
2023-09-12 17:24:43 +0530

    change in file name

--- textExtract/models.py
+++ textExtract/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
+def upload_location(instance, filename):
+    extension=filename.split()[-1]
+    # print(filename,extension)
+    # print(instance.name)
+    return 'assets/%s' % (instance.name)
+
 class CardData(models.Model):
     name=models.CharField(max_length=255,null=False, blank=False)
     number=models.CharField(max_length=255, null=False, blank=False)
     email=models.EmailField(max_length=255, null=False, blank=False)
     address=models.TextField()
-    image=models.ImageField(upload_to='assets/')
+    image=models.ImageField(upload_to=upload_location)
 
     
     def __str__(self):
