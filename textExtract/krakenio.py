7a55bcd04db7857bf78ec050c237ddc9de5c9ba4
shivansh <shivanshkate@gmail.com>
2023-09-17 17:19:48 +0530

    change in view

---
+++ textExtract/krakenio.py
@@ -0,0 +1,14 @@
+# from krakenio import Client
+
+# api = Client('your_api_key', 'your_api_secret')
+
+# data = {
+#     'wait': True
+# }
+
+# result = api.upload('/path/to/image/file.jpg', data);
+
+# if result.get('success'):
+#     print(result.get('kraked_url'))
+# else:
+#     print(result.get('message'))
--- textExtract/views.py
+++ textExtract/views.py
@@ -3,6 +3,7 @@ from .models import CardData
 from .serializers import ImageUploadSerializer
 from rest_framework.response import Response
 from decouple import config
+from django.contrib.sites.shortcuts import get_current_site
 from azure.core.exceptions import ResourceNotFoundError
 from azure.core.credentials import AzureKeyCredential
 from azure.ai.formrecognizer import FormRecognizerClient
@@ -27,117 +28,139 @@ class TextExtractViewSet(generics.ListAPIView):
         # print(file_name)
         obj=CardData.objects.create(image=file,name=file_name)
         obj.save()
+        print(obj.image.url)
         image_url ='./media/assets/'+str(file_name)
         # image_url ='assets/'+str(file_name)
+        current_site=get_current_site(request).domain
+        formUrl =str(current_site)+image_url
+        print(formUrl)
         form_recognizer_client = FormRecognizerClient(
             endpoint=ENDPOINT, credential=AzureKeyCredential(API_KEY))
         with open(image_url, "rb") as f:
             try:
-                poller = form_recognizer_client.begin_recognize_business_cards(
-                    business_card=f, locale="en-US")
+
+                # poller = form_recognizer_client.begin_recognize_business_cards(
+                poller = form_recognizer_client.begin_analyze_document_from_url("prebuilt-businessCard", formUrl)
                 business_cards = poller.result()
-            # print(business_cards)
             except:
                 return Response({"error_message":"Unable to detect Card. Please place the card properly"})
-        for idx, business_card in enumerate(business_cards):
-            # print("--------Recognizing business card #{}--------".format(idx +
-            #                                                              1))
-            contact_names = business_card.fields.get("ContactNames")
-            if contact_names:
-                for contact_name in contact_names.value:
-                    name=contact_name.value["FirstName"].value+contact_name.value["LastName"].value
-                    # print("Contact Name: {}{}".format(
-                    #     contact_name.value["FirstName"].value,contact_name.value["LastName"].value ))
-            else:
-                name=''
-        
-            company_names = business_card.fields.get("CompanyNames")
-            if company_names:
-                for company_name in company_names.value:
-                    company=company_name.value
-                    # print("Company Name: {}".format(
-                    #     company_name.value))
-            else:
-                company=''
-            departments = business_card.fields.get("Departments")
-            if departments:
-                for department in departments.value:
-                    dprmt=department.value
-                    # print("Department: {}".format(
-                    #     department.value))
-            else:
-                dprmt=''
-            job_titles = business_card.fields.get("JobTitles")
-            if job_titles:
-                for job_title in job_titles.value:
-                    job=job_title.value
-                    # print("Job Title: {}".format(
-                    #     job_title.value))
-            else:
-                job=''
-            emails = business_card.fields.get("Emails")
-            if emails:
-                for email in emails.value:
-                    mail=email.value
-                    # print("Email: {}".format(
-                    #     email.value))
-            else:
-                mail=''
-            websites = business_card.fields.get("Websites")
-            if websites:
-                for website in websites.value:
-                    site=website.value
-                    # print("Website: {}".format(
-                    #     website.value))
-            else:
-                site=''
-            addresses = business_card.fields.get("Addresses")
-            if addresses:
-                for address in addresses.value:
-                    add=address.value
-                    # print("Address: {}".format(
-                    #     address.value))
-            else:
-                add=''
-            mobile_phones = business_card.fields.get("MobilePhones")
-            if mobile_phones:
-                for phone in mobile_phones.value:
-                    number=phone.value
-                    # print("Mobile phone number: {}".format(
-                    #     phone.value))
-            else:
-                number=''
-            faxes = business_card.fields.get("Faxes")
-            if faxes:
-                for fax in faxes.value:
-                    faxNum=fax.value
-                    # print("Fax number: {}".format(
-                    #     fax.value))
-            else:
-                faxNum=''
-            work_phones = business_card.fields.get("WorkPhones")
-            if work_phones:
-                for work_phone in work_phones.value:
-                    workPhone=work_phone.value
-                    # print("Work phone number: {}".format(
-                    #     work_phone.value))
-            other_phones = business_card.fields.get("OtherPhones")
-            if other_phones:
-                for other_phone in other_phones.value:
-                    otherPhone=other_phone.value
-                    # print("Other phone number: {}".format(
-                    #     other_phone.value))
-            else:
-                other_phone=''
+            print(business_cards[0].fields.keys())
+            for idx, business_card in enumerate(business_cards.documents):
+                print("--------Analyzing business card #{}--------".format(idx + 1))
+                contact_names = business_card.fields.get("ContactNames")
+                if contact_names:
+                    for contact_name in contact_names.value:
+                        contact_names= contact_name.value["FirstName"].value + contact_name.value["LastName"].value
+                        print(
+                            "Contact Name: {} {}".format(
+                                contact_name.value["FirstName"].value,
+                                contact_name.value["LastName"].value
+                                # contact_name.value[
+                                #     "FirstName"
+                                # ].confidence,
+                            )
+                        )
+                    
+                company_names = business_card.fields.get("CompanyNames")
+                if company_names:
+                    for company_name in company_names.value:
+                        company_names=company_name.value
+                        print(
+                            "Company Name: {} ".format(
+                                company_name.value, #company_name.confidence
+                            )
+                        )
+                departments = business_card.fields.get("Departments")
+                if departments:
+                    for department in departments.value:
+                        departments=department.value
+                        print(
+                            "Department: {} ".format(
+                                department.value, #department.confidence
+                            )
+                        )
+                job_titles = business_card.fields.get("JobTitles")
+                if job_titles:
+                    for job_title in job_titles.value:
+                        job_titles=job_title.value
+                        print(
+                            "Job Title: {}".format(
+                                job_title.value,# job_title.confidence
+                            )
+                        )
+                emails = business_card.fields.get("Emails")
+                if emails:
+                    for email in emails.value:
+                        emails=email.value
+                        print(
+                            "Email: {} ".format(email.value, #email.confidence 
+                                                                )
+                        )
+                websites = business_card.fields.get("Websites")
+                if websites:
+                    for website in websites.value:
+                        websites=website.value
+                        print(
+                            "Website: {} ".format(
+                                website.value
+                            )
+                        )
+                addresses = business_card.fields.get("Addresses")
+                if addresses:
+                    for address in addresses.value:
+                        addresses=address.value
+                        print(
+                            "Address: {} ".format(
+                                address.value
+                            )
+                        )
+                mobile_phones = business_card.fields.get("MobilePhones")
+                if mobile_phones:
+                    for phone in mobile_phones.value:
+                        mobile_phones=phone.content
+                        print(
+                            "Mobile phone number: {} ".format(
+                                phone.content
+                            )
+                        )
+                faxes = business_card.fields.get("Faxes")
+                if faxes:
+                    for fax in faxes.value:
+                        faxes=fax.content
+                        print(
+                            "Fax number: {} ".format(
+                                fax.content
+                            )
+                        )
+                work_phones = business_card.fields.get("WorkPhones")
+                if work_phones:
+                    for work_phone in work_phones.value:
+                        work_phones=work_phone.content
+                        print(
+                            "Work phone number: {} ".format(
+                                work_phone.content
+                            )
+                        )
+                other_phones = business_card.fields.get("OtherPhones")
+                if other_phones:
+                    for other_phone in other_phones.value:
+                        other_phones=other_phone.value
+                        print(
+                            "Other phone number: {} ".format(
+                                other_phone.value
+                            )
+                        )
+                print("----------------------------------------")
             card_data={
-                "Name":name,
-                "Company Name":company,
-                "Department":dprmt,
-                "Email":mail,
-                "Fax":faxNum,
-                "Work Number":number,
-                "Website":site,
-                "Address":add
+                "Name":contact_names,
+                "Company Name":company_names,
+                "Department":departments,
+                "Job Titles":job_titles,
+                "Email":emails,
+                "Fax":faxes,
+                "Work Number":work_phones,
+                "Website":websites,
+                "Address":addresses
             }
             data={
                 "status code":200,
