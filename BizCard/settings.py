96b28052d0e2f95c386af093934d5635806d2282
Shiranka23 <142572986+Shiranka23@users.noreply.github.com>
2023-09-18 15:48:26 +0530

    Update settings.py

--- BizCard/settings.py
+++ BizCard/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ["*"]
 
