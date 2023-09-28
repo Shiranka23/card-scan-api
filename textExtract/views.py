128bb65d3eff2c06c0d4576cc373ca75525783e6
shivansh <shivanshkate@gmail.com>
2023-09-28 12:09:56 +0530

    increase in upload file size

--- textExtract/views.py
+++ textExtract/views.py
@@ -22,7 +22,6 @@ from .models import CardData
 from .serializers import ImageUploadSerializer
 from .decoder import decode_base64_file
 
-
 # Credentials
 API_KEY = config("AZURE_API_KEY")
 ENDPOINT = config("AZURE_ENDPOINT")
@@ -33,7 +32,7 @@ def get( request, file):
         with open(file_location, 'rb') as f:
             file_data = f.read()
             ext = file.split(".")[-1]
-            print(ext)
+            # print(ext)
             response = HttpResponse(content=file_data, content_type=f'image/{ext}')
             return response
     except IOError:
@@ -45,40 +44,8 @@ class TextExtractViewSet(generics.ListAPIView):
     queryset = CardData.objects.all()
     serializer_class = ImageUploadSerializer
 
-
-    # def decode_base64_file(data):
-
-    #     def get_file_extension(file_name, decoded_file):
-    #         extension=imghdr.what(file_name, decoded_file)
-    #         extension='jpg' if extension=='jpeg' else extension
-    #         return extension
-        
-    #      # Check if this is a base64 string
-    #     if isinstance(data, six.string_types):
-    #         # Check if the base64 string is in the "data:" format
-    #         if 'data:' in data and ';base64,' in data:
-    #             # Break out the header from the base64 content
-    #             header, data=data.split(';base64,')
-
-    #         # try to decode the file. Return validation error if is false
-    #         try:
-    #             decoded_file=b64decode(data)
-    #         except TypeError:
-    #             TypeError('invalid_image')
-            
-    #         # generate filename
-    #         file_name=str(uuid4.uuid4())[:12]
-
-    #         # get the file extansion
-    #         file_extension=get_file_extension(file_name,decoded_file)
-    #         complete_file_name = "%s.%s" % (file_name, file_extension)
-
-    #         file=ContentFile(decoded_file, name=complete_file_name)
-    #         return file,complete_file_name
-
     def post(self, request, *args, **kwargs):
         
-        # print(image)
         try:
             # Get base64 image string and generate a unique filename
             data=json.loads(request.body.decode('utf-8'))
@@ -92,11 +59,7 @@ class TextExtractViewSet(generics.ListAPIView):
 
             # creating URL of the uploaded image
             formUrls = f'http://{current_site}{image_url}'
-            print(formUrls)
-
-            #  sample docs
-            # formUrl = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-REST-api-samples/master/curl/form-recognizer/business-card-english.jpg"
-           
+            # print(formUrls)
 
             document_analysis_client = DocumentAnalysisClient(
                 endpoint=ENDPOINT, credential=AzureKeyCredential(API_KEY)
@@ -104,15 +67,11 @@ class TextExtractViewSet(generics.ListAPIView):
 
             poller = document_analysis_client.begin_analyze_document_from_url("prebuilt-businessCard", formUrls)
             business_cards = poller.result()
-            # print(business_cards.documents)
 
             instance=CardData.objects.get(name=file_name)
             instance.image.delete()
             instance.delete()
-            # instance=CardData.objects.get(name=file_name)
-            # print('instance')
 
-            card_data = []
             phone_number=[]
             
             # Extract information from the business card
@@ -121,8 +80,6 @@ class TextExtractViewSet(generics.ListAPIView):
                 contact_names = business_card.fields.get("ContactNames")
                 if contact_names:
                     for contact_name in contact_names.value:
-                        # firstname=[contact_name.value["FirstName"].value if contact_name.value["FirstName"].value else []]
-                        # lastname=[contact_name.value["LastName"].value if contact_name.value["LastName"].value else []]
                         if contact_name.value["FirstName"]:
                             # print("firstname: ",contact_name.value["FirstName"].value)
                             firstname=contact_name.value["FirstName"].value
@@ -136,14 +93,16 @@ class TextExtractViewSet(generics.ListAPIView):
                         name=firstname+" "+lastname
                 else:
                     name=' '
+
                 company_names = business_card.fields.get("CompanyNames")
                 if company_names:
                     company=[company_name.value for company_name in company_names.value]
                     # for company_name in company_names.value:
-                    # print("company: ",company_name.value)
+                        # print("company: ",company_name.value)
                         # company = company_name.value
                 else:
                     company=''
+
                 departments = business_card.fields.get("Departments")
                 if departments:
                     dprmt=[department.value for department in departments.value]
@@ -152,6 +111,7 @@ class TextExtractViewSet(generics.ListAPIView):
                         # dprmt=department.value
                 else:
                     dprmt=[]
+
                 job_titles = business_card.fields.get("JobTitles")
                 if job_titles:
                     job=[job_title.value for job_title in job_titles.value]
@@ -160,6 +120,7 @@ class TextExtractViewSet(generics.ListAPIView):
                         # job=job_title.value
                 else:
                     job=" "
+
                 emails = business_card.fields.get("Emails")
                 if emails:
                     mail=[email.value for email in emails.value]
@@ -168,6 +129,8 @@ class TextExtractViewSet(generics.ListAPIView):
                         # mail=email.value
                 else:
                     mail=[]
+
+
                 websites = business_card.fields.get("Websites")
                 if websites:
                     site=[website.value for website in websites.value]
@@ -176,6 +139,7 @@ class TextExtractViewSet(generics.ListAPIView):
                         # site=website.value
                 else:
                     site=[]
+
                 addresses = business_card.fields.get("Addresses")
                 if addresses:
                     for address in addresses.value:
@@ -183,6 +147,7 @@ class TextExtractViewSet(generics.ListAPIView):
                         add=address.content
                 else:
                     add=" "
+
                 mobile_phones = business_card.fields.get("MobilePhones")
                 if mobile_phones:
                     for phone in mobile_phones.value:
@@ -190,6 +155,7 @@ class TextExtractViewSet(generics.ListAPIView):
                         phone_number.append(phone.content)
                 else:
                     phoneNum=[]
+
                 faxes = business_card.fields.get("Faxes")
                 if faxes:
                     faxNum=[fax.content for fax in faxes.value]
@@ -198,6 +164,7 @@ class TextExtractViewSet(generics.ListAPIView):
                         # faxNum=fax.content
                 else:
                     faxNum=[]
+
                 work_phones = business_card.fields.get("WorkPhones")
                 if work_phones:
                     for work_phone in work_phones.value:
@@ -205,6 +172,7 @@ class TextExtractViewSet(generics.ListAPIView):
                         phone_number.append(work_phone.content)
                 else:
                     workPhone=[]
+
                 other_phones = business_card.fields.get("OtherPhones")
                 if other_phones:
                     for other_phone in other_phones.value:
@@ -213,19 +181,18 @@ class TextExtractViewSet(generics.ListAPIView):
                 else:
                     otherPhone=[]
                
-            card_info={
+            card_data={
                 "name":name,
                 "company":company,
                 "address":add,
-                "Phone numbers":phone_number,
-                "fax ":faxNum,
+                "phoneNumbers":phone_number,
+                "fax":faxNum,
                 "email":mail,
                 "job":job,
                 "department":dprmt,
                 "website":site,
             }
             
-            card_data.append(card_info)
             response_data = {
                 "status_code": 200,
                 "message": "Success",
@@ -234,5 +201,5 @@ class TextExtractViewSet(generics.ListAPIView):
             return Response(response_data)
         except Exception as e:
             error_message = str(e)
-            print(error_message)
+            # print(error_message)
             return Response({"error_message": error_message}, status=500)
